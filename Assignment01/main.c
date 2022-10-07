ec8695e13f6935d6312066e4db444676eb7d43d6
Florian HEMBERT <flhember@e1r3p2.clusters.42paris.fr>
2022-10-07 16:45:34 +0200

    Ex 0 to 3 done

---
+++ Assignment01/main.c
@@ -0,0 +1,20 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("flhember");
+MODULE_DESCRIPTION("Hello world module");
+
+static int __init ModuleInit(void)
+{
+	pr_info("Hello world !\n");
+	return 0;
+}
+
+static void __exit ModuleExit(void)
+{
+	pr_info("Cleaning up module.\n");
+}
+
+module_init(ModuleInit);
+module_exit(ModuleExit);
